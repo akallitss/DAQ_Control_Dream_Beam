eb2e042f947cdcd608580ec522022899ad21fe2c
Dylan Neff <Dyn0402@hotmail.com>
2026-05-14 13:12:24 +0200

    run_config_beam.py set to 100 samples and zero suppressed

--- run_config_beam.py
+++ run_config_beam.py
@@ -66,12 +66,12 @@ class Config(RunConfigBase):
             # 'n_samples_per_waveform': 450,  # Number of samples per waveform to configure in DAQ
             # 'n_samples_per_waveform': 400,  # Number of samples per waveform to configure in DAQ
             # 'n_samples_per_waveform': 32,  # Number of samples per waveform to configure in DAQ
-            'n_samples_per_waveform': 150,  # Number of samples per waveform to configure in DAQ
+            'n_samples_per_waveform': 100,  # Number of samples per waveform to configure in DAQ
             'go_timeout': 5 * 60,  # Seconds to wait for 'Go' response from RunCtrl before assuming failure
             'max_run_time_addition': 60 * 5,  # Seconds to add to requested run time before killing run
             'copy_on_fly': True,  # True to copy raw data to out dir during run, False to copy after run
             'batch_mode': True,  # Run Dream RunCtrl in batch mode. Not implemented for cosmic bench CPU.
-            'zero_suppress': False,  # True to run in zero suppression mode, False to run in full readout mode
+            'zero_suppress': True,  # True to run in zero suppression mode, False to run in full readout mode
             'pedestals_dir': f'{self.base_out_dir}pedestals/',  # None to ignore, else top directory for pedestal runs
             'pedestals': 'latest',  # 'latest' for most recent, otherwise specify directory name, eg "pedestals_10-22-25_13-43-34"
             # 'latency': 90,  # Latency setting for DAQ in clock cycles
